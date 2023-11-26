bdda923bd495755b7c5d68ec5c1f0da0bab9ea3a
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2023-11-26 23:23:54 +0200

    Update managers.py

--- Exam_preparation_II/main_app/managers.py
+++ Exam_preparation_II/main_app/managers.py
@@ -4,6 +4,7 @@ from django.db.models import Count
 
 class ProfileManager(models.Manager):
     def get_regular_customers(self):
+       
         return self.annotate(
             orders_count=Count('orders')
         ).filter(
